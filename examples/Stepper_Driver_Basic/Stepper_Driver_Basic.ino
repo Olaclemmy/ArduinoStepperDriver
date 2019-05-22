b9e2fb09e8848b250c87b228ef8f781af67ced32
openbuilds-engineer <p.walt@openbuilds.com>
2019-05-22 22:21:07 +0200

    update

--- examples/Stepper_Driver_Basic/Stepper_Driver_Basic.ino
+++ examples/Stepper_Driver_Basic/Stepper_Driver_Basic.ino
@@ -5,7 +5,6 @@ STPDRIVER driver1;
 void setup(){
 
   Serial.begin(115200);
-
   //Configure Pins (enable, step, direction)
   driver1.pinConfig(8,2,5);
 
@@ -18,20 +17,22 @@ void setup(){
   //Define Steps per millimeter
   driver1.stepPerMm(100);
 
-  // Setup Acceleration:  acceleration value (Must be less than Maximum Speed) , maximum SPEED IN MM/MIN
-  driver1.motionConfig(10,10000);
+  // Setup Acceleration:  acceleration , maximum SPEED IN MM/MIN
+  driver1.motionConfig(10,5000);
 
   //Define Endstop pins and mode (min, max, logic level triggered)
-  // Not Implemented Yet!
   driver1.endstopConfig(9,9, HIGH);
 
 }
 
 void loop(){
-//  Serial.println("Moving 50mm forward then back");
+  //Moving 50mm forward then back
   driver1.motorMoveTo(50, true);
-
   driver1.motorMoveTo(50, false);
+
+  // spin 10 rotations one way then 10 rotations back
+  driver1.motorMoveRotations(10, true, 2000);
+  driver1.motorMoveRotations(10, false, 5000);
 
 
 }
